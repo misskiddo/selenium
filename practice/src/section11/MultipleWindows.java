61ef78a7cb5e100ed367630966bcf747c6b485d6
Toñi <misskiddo@hotmail.com>
2020-02-28 14:28:55 -0500

    Actions and multiple windows

---
+++ practice/src/section11/MultipleWindows.java
@@ -0,0 +1,41 @@
+package section11;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MultipleWindows {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://accounts.google.com/");
+		
+		
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.linkText("Help")).click();
+		
+		Set<String> ids= driver.getWindowHandles();
+		
+		Iterator<String> it = ids.iterator();
+		
+		// Go to the parent window
+		String parent = it.next();
+			
+		// Go to the child
+		String child = it.next();
+		
+		driver.switchTo().window(child);
+	
+		System.out.println(driver.getTitle());
+		
+		
+		
+	}
+
+}
